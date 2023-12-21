529eec9b49a0c2f4e2d100a8769f2feb34cea342
Bruno <121259934+amatbruno@users.noreply.github.com>
2023-12-21 16:51:32 +0100

    .

--- public/game/game.js
+++ public/game/game.js
@@ -3,21 +3,40 @@
     const $canvas = document.querySelector(".c1");
     const context = $canvas.getContext('2d');
     const socket = io();
+    const dots = [];
 
-    const drawPoint = (x, y) => {
-        context.strokeStyle = "red";
-        context.lineWidth = 5;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x + 1, y + 1);
-        context.stroke();
+    const drawPoint = (x, y, clicked) => {
+        dots.push({ x, y, clicked })
     }
 
-    socket.on('draw', ({ x, y }) => {
-        drawPoint(x, y)
+    const reDraw = (x, y) => {
+        context.clearRect(x, y, $canvas.width, $canvas.height);
+        if (dots.length < 2) return requestAnimationFrame(reDraw);
+
+
+        dots.forEach(({ x, y, clicked }, index) => {
+            if (!clicked || !index) return;
+            const { x: previousX, y: previousY } = dots[index - 1]
+            context.strokeStyle = 'black';
+            context.lineJoin = 'round';
+            context.lineWidth = 5;
+
+            context.beginPath();
+            context.moveTo(previousX, previousY);
+            context.lineTo(x, y);
+            context.closePath();
+            context.stroke();
+        })
+        requestAnimationFrame(reDraw)
+    }
+
+requestAnimationFrame(reDraw)
+
+    socket.on('draw', ({ x, y, clicked }) => {
+        drawPoint(x, y, clicked)
     })
 
     fetch("/points")
         .then(res => res.json())
-        .then(points => points.forEach(({ x, y }) => drawPoint( x, y ))) 
+        .then(points => points.forEach(({ x, y }) => drawPoint(x, y)))
 })()
--- public/main.js
+++ public/main.js
@@ -3,26 +3,42 @@
     const $canvas = document.querySelector(".c1");
     const context = $canvas.getContext('2d');
     const socket = io();
+    const dots = [];
 
-    const drawPoint = (x, y) => {
-        context.strokeStyle = "red";
-        context.lineWidth = 5;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x + 1, y + 1);
-        context.stroke();
+    const drawPoint = (x, y, clicked) => {
+        dots.push({ x, y, clicked })
     }
 
+    const reDraw = (x, y) => {
+        context.clearRect(x, y, $canvas.width, $canvas.height);
+        if (dots.length < 2) return requestAnimationFrame(reDraw);
+
+
+        dots.forEach(({ x, y, clicked }, index) => {
+            if (!clicked || !index) return;
+            const { x: previousX, y: previousY } = dots[index - 1]
+            context.strokeStyle = 'black';
+            context.lineJoin = 'round';
+            context.lineWidth = 5;
+
+            context.beginPath();
+            context.moveTo(previousX, previousY);
+            context.lineTo(x, y);
+            context.closePath();
+            context.stroke();
+        })
+        requestAnimationFrame(reDraw)
+    }
+
+requestAnimationFrame(reDraw)
+
     $canvas.addEventListener("mousemove", e => {
-        if (e.buttons !== 1) return; {
-            socket.emit("hola")
+        const { top, left } = $canvas.getBoundingClientRect();
+        const y = e.pageY - top
+        const x = e.pageX - left
+        const clicked = e.buttons === 1;
 
-            const { top, left } = $canvas.getBoundingClientRect();
-            const y = e.pageY - top
-            const x = e.pageX - left
-
-            socket.emit('draw', { x, y })
-            drawPoint(x, y)
-        }
+        socket.emit('draw', { x, y, clicked})
+        drawPoint(x, y, clicked)
     })
 })()
