8498bd6b65af5de408722fbbb022dbfe8893f32c
Bruno <brunoamagar@campus.monlau.com>
2024-01-10 19:34:16 +0100

    HTML CSS updates

--- public/snow.js
+++ public/snow.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const snowflake = document.createElement('div');
         snowflake.classList.add('snowflake');
         snowflake.style.left = `${Math.random() * 100}vw`; // Posición horizontal aleatoria
-        snowflake.style.animationDuration = `${Math.random() * 3 + 3.5}s`; // Duración de la animación aleatoria
+        snowflake.style.animationDuration = `${Math.random() * 3 + 7}s`; // Duración de la animación aleatoria
         snowContainer.appendChild(snowflake);
     }
 });
